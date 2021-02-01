367cf5238eaaeedbc5fbde26e9b3e1e23351211f
kelseyjdickson <kelseymedved@gmail.com>
2021-02-01 16:34:22 -0500

    added bones to files

---
+++ src/Components/CardInfo/CardInfo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CardInfo = () => {
+  return <h1>CardInfo</h1>;
+};
+
+export default CardInfo;
---
+++ src/Components/MainContainer/MainContainer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MainContainer = () => {
+  return <h1>MainContainer</h1>;
+};
+
+export default MainContainer;
