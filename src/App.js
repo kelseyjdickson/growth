650e83b13dae2a01589a40578361b276d2fae161
kelseyjdickson <kelseymedved@gmail.com>
2021-02-02 11:32:38 -0500

    structure on app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import Nav from "./Components/Nav/Nav";
+import Banner from "./Components/Banner/Banner";
+import MainContainer from "./Components/MainContainer/MainContainer";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
     <div className="App">
       <h1>I am app</h1>
+      <Nav />
+      <Banner />
+      <MainContainer />
+      <Footer />
     </div>
   );
 }
