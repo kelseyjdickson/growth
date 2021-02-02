9bc5ad78d91414fbf10dbe305c02cefbe05b41b4
kelseyjdickson <kelseymedved@gmail.com>
2021-02-02 12:09:42 -0500

    return parens

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -2,13 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Nav = () => {
-  return <h1>Nav</h1>;
+  return (
+    <>
+      <h1>Nav</h1>
 
-  <ul>
-    <Link to="/about">About</Link>
-    <Link to="/resoures">Resources</Link>
-    <Link to="/contact">Contact</Link>
-  </ul>;
+      <ul>
+        <Link to="/about">About</Link>
+        <Link to="/resoures">Resources</Link>
+        <Link to="/contact">Contact</Link>
+      </ul>
+    </>
+  );
 };
 
 export default Nav;
