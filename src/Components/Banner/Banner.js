3c75419795a3a5b7c54e0366a3566a0c83aa4ee2
kelseyjdickson <kelseymedved@gmail.com>
2021-02-16 07:41:16 -0500

    added photos

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from 'styled-components';
+import banner from './womenintech.jpeg'
+
+const BannerImage = styled.img`
+  width: 600px;
+  height: 400px;
+
+`
 
 const Banner = () => {
-  return <h1>Banner</h1>;
+  return (
+
+<BannerImage src={banner}alt="women in tech"></BannerImage>
+
+  )
 };
 
 export default Banner;
--- src/Components/MainContainer/MainContainer.js
+++ src/Components/MainContainer/MainContainer.js
@@ -1,37 +1,48 @@
 import React from "react";
 import { diffLinesRaw } from "jest-diff";
 import { isTemplateElement } from "@babel/types";
+import kristal from './kristal.png'
+import erika from './erika.png'
+import kimberly from './kimberly.png'
+import joy from './joy.png'
+import reshma from './reshma.png'
+
 
 const womenData = [ {
   name: 'Kristal Garcia',
   company: "Year One",
   job_title: 'Chief Strategy Officer',
-  contact_link: 'https://www.linkedin.com/in/kgarcia4/'
+  contact_link: 'https://www.linkedin.com/in/kgarcia4/',
+  image: kristal
 
 },
 {
 name: 'Erica Joy Baker',
 company: "Github",
 job_title: 'Director of Engineering',
-contact_link: 'https://www.linkedin.com/in/ericajoy/'
+contact_link: 'https://www.linkedin.com/in/ericajoy/',
+image: erika
 },
 {
 name: 'Joy Buolamwini',
 company: "Algorithmic Justice League",
 job_title: 'Computer Scientist - Poet of Code',
-contact_link: 'https://www.linkedin.com/in/buolamwini/'
+contact_link: 'https://www.linkedin.com/in/buolamwini/',
+image: joy
 },
 {
   name: 'Kimberly Bryant',
   company: "Black Girls Code",
   job_title: 'Founder and CEO of Black Girls Code',
-  contact_link: 'https://www.linkedin.com/in/kimberlybryant/'
+  contact_link: 'https://www.linkedin.com/in/kimberlybryant/',
+  image: kimberly
   },
   {
     name: 'Reshma Saujani',
     company: "Girls Who Code",
     job_title: 'Founder of Girls Who Code',
-    contact_link: 'https://www.linkedin.com/in/reshma-saujani/'
+    contact_link: 'https://www.linkedin.com/in/reshma-saujani/',
+    image: reshma
     }
 ] 
 
@@ -47,6 +58,7 @@ let females = womenData.map((info) => {
     <h2>Company:{info.company}</h2>
     <h2>Job Title: {info.job_title}</h2>
     <a href={info.contact_link}/>
+    <img src={info.image}alt="powerful ladies"/>
   </div>
 })
   
@@ -55,7 +67,7 @@ let females = womenData.map((info) => {
   return (
     
     <div>
-    <h1>MainContainer</h1>
+    
        {females}
     </div>
       
