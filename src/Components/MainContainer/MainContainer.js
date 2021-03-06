6e7c2d4f3890e9765a0727350e4d56d45f17cf05
kelseyjdickson <kelseymedved@gmail.com>
2021-03-06 11:09:08 -0500

    new Styling

--- src/Components/MainContainer/MainContainer.js
+++ src/Components/MainContainer/MainContainer.js
@@ -8,6 +8,10 @@ import reshma from './reshma.png'
 import InfiniteScroll from 'react-infinite-scroll-component';
 import { readFileSync } from "fs";
 
+// Styles
+import { Card, Grid, GridColumn } from 'semantic-ui-react'
+import styled from 'styled-components'
+
 
 const womenData = [ {
   name: 'Kristal Garcia',
@@ -44,60 +48,45 @@ image: joy
     job_title: 'Founder of Girls Who Code',
     contact_link: 'https://www.linkedin.com/in/reshma-saujani/',
     image: reshma
-    },
-    {
-    name: 'Kristal Garcia',
-    company: "Year One",
-    job_title: 'Chief Strategy Officer',
-    contact_link: 'https://www.linkedin.com/in/kgarcia4/',
-    image: kristal
-  
-  },
-  {
-  name: 'Erica Joy Baker',
-  company: "Github",
-  job_title: 'Director of Engineering',
-  contact_link: 'https://www.linkedin.com/in/ericajoy/',
-  image: erika
-  },
-  {
-  name: 'Joy Buolamwini',
-  company: "Algorithmic Justice League",
-  job_title: 'Computer Scientist - Poet of Code',
-  contact_link: 'https://www.linkedin.com/in/buolamwini/',
-  image: joy
-  },
-  {
-    name: 'Kimberly Bryant',
-    company: "Black Girls Code",
-    job_title: 'Founder and CEO of Black Girls Code',
-    contact_link: 'https://www.linkedin.com/in/kimberlybryant/',
-    image: kimberly
-    },
-    {
-      name: 'Reshma Saujani',
-      company: "Girls Who Code",
-      job_title: 'Founder of Girls Who Code',
-      contact_link: 'https://www.linkedin.com/in/reshma-saujani/',
-      image: reshma
-      }
+    }
+ 
 ] 
 
 
 
+
+const StyledDiv = styled.div`
+  display: flex;
+   flex-direction: row;
+  justify-content: space-between;
+  width:100%;
+  height: auto;
+
+`
 const MainContainer = () => {
   
-
+ 
 
 let females = womenData.map((info) => {
-  return <div key={info.id}>
-    <h1>Name:{info.name}</h1>
-    <h2>Company:{info.company}</h2>
-    <h2>Job Title: {info.job_title}</h2>
-    <a href={info.contact_link} alt="contact"/>
-    <img src={info.image}alt="powerful ladies"/>
-  </div>
-})
+  return <Card color="red"
+    style={{
+      backgroundColor: "#FAA35C",
+      
+      
+    }} 
+    color="yellow"
+    header="pink"
+    key={info.id}
+    
+    header={info.name} 
+   
+    meta={info.company}
+    description={info.job_title}
+    image={info.image}
+    href={info.contact_link}
+    />
+   
+  })
   
   
    
@@ -112,16 +101,16 @@ let females = womenData.map((info) => {
     //     </p>
     //   }>
     
-    <div>
 
-
+<StyledDiv>
     
        {females}
-   
   
-     
 
-    </div>
+       </StyledDiv>
+   
+
+   
       
     // </InfiniteScroll>
   )
