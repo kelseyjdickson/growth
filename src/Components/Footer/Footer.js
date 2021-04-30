350b44b4a68fa08ca65c6bb43504eae772dbd062
kelseyjdickson <kelseymedved@gmail.com>
2021-04-30 08:03:21 -0400

    footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,7 +1,16 @@
 import React from "react";
+import './Footer.scss'
 
 const Footer = () => {
-  return <h1>Footer</h1>;
+  
+  return (
+    <div className="container">
+    <div className="column-1">
+      <h6>About</h6>
+    </div>
+
+  </div>
+    )
 };
 
 export default Footer;
